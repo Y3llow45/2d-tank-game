9693a0ee591dc2559b8e983c7038a5ace145ec0e
Yellow <bofengejn@gmail.com>
2023-03-10 19:11:54 +0200

    added mechanics

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
+
 var ballRadius = 10;
 var x = canvas.width / 2;
 var y = canvas.height - 30;
+var t1x = canvas.width / 4;
+var t1y = canvas.height - 50;
+
 var dx = 3;
 var dy = -3;
-const paddleHeight = 70;
-const paddleWidth = 75;
+const paddleHeight = 48;
+const paddleWidth = 68;
+const t1Height = 48;
+const t1Width = 70;
 let paddleX = (canvas.width - paddleWidth) / 2;
 let paddleY = (canvas.height - paddleHeight) / 2;
 let rightPressed = false;
@@ -21,8 +27,8 @@ document.addEventListener("keyup", keyUpHandler, false);
 
 function make_base() {
     let base_image = new Image();
-    base_image.src = 'tank1_p2.png';
-    ctx.drawImage(base_image, 400, 400);
+    base_image.src = 'tank1_p.png';
+    ctx.drawImage(base_image, t1x, t1y);
 }
 
 function keyDownHandler(e) {
@@ -82,12 +88,16 @@ function draw() {
     }*/
     if (rightPressed) {
         paddleX = Math.min(paddleX + 7, canvas.width - paddleWidth);
+        t1x = Math.min(t1x + 7, canvas.width - t1Width);
     } if (leftPressed) {
         paddleX = Math.max(paddleX - 7, 0);
+        t1x = Math.max(t1x - 7, 0);
     } if (upPressed) {
         paddleY = Math.max(paddleY - 7, 0);
+        t1y = Math.max(t1y - 7, 0);
     } if (downPressed) {
         paddleY = Math.min(paddleY + 7, canvas.height - paddleHeight);
+        t1y = Math.min(t1y + 7, canvas.height - t1Height);
     }
 
 
